b2418cc14c85d020de35d586b315e88e04a4f0d1
rumpapl <rumpakucse14@gmail.com>
2021-10-07 13:04:18 +0600

    object-prototype

---
+++ JS Objects/prototype.js
@@ -0,0 +1,48 @@
+function Person(name, age, country) {
+    this.name = name,
+    this.age = age,
+    this.country = country
+};
+
+console.dir(Person);
+
+Person.prototype ={
+    sleep(){
+        console.log('Sleep Time!');
+    }
+}
+
+
+const korim = new Person('Karim', 27, 'BD');
+console.log(korim);
+korim.sleep();
+
+// adding eat method to person prototype
+Person.prototype={
+    // to add existing prototype
+    ...Person.prototype,
+    eat(){
+        console.log('Time to eat!');
+    }
+}
+
+const rohim = new Person('Rohim', 27, 'BD');
+console.log(rohim);
+rohim.sleep();
+rohim.eat();
+
+Person.prototype={
+    ...Person.prototype,
+    eyeColor:'green',
+}
+
+const salam = new Person('Salam', 33, 'UK');
+console.dir(salam);
+console.log(salam.name);
+console.log(salam.age);
+console.log(salam.country);
+console.log(salam.eyeColor);
+salam.eat();
+salam.sleep();
+
+
