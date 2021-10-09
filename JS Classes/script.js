bb4e6390e63e13930cf0d386066f6e0c959fef04
rumpapl <rumpakucse14@gmail.com>
2021-10-09 19:00:37 +0600

    js class-inheritance conpect

--- JS Classes/script.js
+++ JS Classes/script.js
@@ -8,6 +8,7 @@ class Cat {
 
     showName() {
         console.log(`Hello, I am ${this.name}.`);
+        return this.name;
     }
 
     showAge(){
@@ -26,3 +27,26 @@ cat1.showName();
 cat1.showAge();
 cat2.showName();
 cat2.showAge();
+
+// Inheritance 
+class Persian extends Cat{
+    constructor(name, age, tailLength){
+        super(name, age);
+        this.tailLength = tailLength
+    }
+
+    tailInfo(){
+        return this.tailLength;
+    }
+    // call parent 
+    show(){
+       return this.name + ' && '+ this.age;
+    }
+}
+
+// Create an onject
+const par_cat1= new Persian('Damo', 7, 10);
+par_cat1.showName();
+par_cat1.showAge();
+console.log(`${par_cat1.name}' tail is ${par_cat1.tailInfo()}inch long.'`);
+console.log(par_cat1.show());
