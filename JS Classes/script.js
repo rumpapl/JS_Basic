ee4ff872c9d6bef4f9be282c087f61faaa68093f
rumpapl <rumpakucse14@gmail.com>
2021-10-09 19:17:33 +0600

    class static method concept

--- JS Classes/script.js
+++ JS Classes/script.js
@@ -6,12 +6,20 @@ class Cat {
             this.age = age
     }
 
+    // static method
+    static greed() {
+        return 'Hello! I am a static method.';
+    }
+
+    static hello(obj){
+        return 'Hello, '+ obj.name;
+    }
     showName() {
         console.log(`Hello, I am ${this.name}.`);
         return this.name;
     }
 
-    showAge(){
+    showAge() {
         console.log(`I am ${this.age} years old.`);
     }
 }
@@ -29,24 +37,46 @@ cat2.showName();
 cat2.showAge();
 
 // Inheritance 
-class Persian extends Cat{
-    constructor(name, age, tailLength){
+class Persian extends Cat {
+    constructor(name, age, tailLength) {
         super(name, age);
         this.tailLength = tailLength
     }
 
-    tailInfo(){
+    tailInfo() {
         return this.tailLength;
     }
     // call parent 
-    show(){
-       return this.name + ' && '+ this.age;
+    show() {
+        return this.name + ' && ' + this.age;
     }
 }
 
 // Create an onject
-const par_cat1= new Persian('Damo', 7, 10);
+const par_cat1 = new Persian('Damo', 7, 10);
 par_cat1.showName();
 par_cat1.showAge();
 console.log(`${par_cat1.name}' tail is ${par_cat1.tailInfo()}inch long.'`);
 console.log(par_cat1.show());
+
+// Class Static 
+// create a object from Cat class
+const cat3 = new Cat('Tin', 1);
+
+// calling methods
+cat3.showName();
+cat3.showAge();
+// console.log(cat3.greed());
+// above method call occures an error, 
+// Cz static method can`t be called from object.
+// Static method only can be called using Calss name, 
+// like below,
+console.log(Cat.greed());
+
+// If you want to use the myCar object inside the static method
+// you can send it as a parameter:
+// ex:
+let msg = Cat.hello(cat3);
+console.log(msg);
+
+
