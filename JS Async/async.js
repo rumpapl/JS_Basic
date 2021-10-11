44c321df96a143b7e6461d2500a46319791d041f
rumpapl <rumpakucse14@gmail.com>
2021-10-11 23:54:31 +0600

    js promise

--- JS Async/async.js
+++ JS Async/async.js
@@ -1,8 +1,8 @@
 // js async 
 console.log('JS Asynchronous:');
 
-function display(param) {
-    document.getElementById('show-html').innerHTML=param;
+function display(param, elementName) {
+    document.getElementById(elementName).innerHTML = param;
 }
 
 function getFileInfo(callbackFun) {
@@ -10,14 +10,41 @@ function getFileInfo(callbackFun) {
     let req = new XMLHttpRequest();
     req.open('GET', "./mycar.html");
     console.dir(req);
-    req.onload=function () {
-        if (req.status === 200){
-            callbackFun(this.responseText);
+    req.onload = function () {
+        if (req.status === 200) {
+            callbackFun(this.responseText, 'show-html');
         }
-        else{
-            callbackFun(`Errors: ${req.status}`);
+        else {
+            callbackFun(`Errors: ${req.status}`, 'show-html');
         }
     }
     req.send();
     console.log('End Function');
 }
+
+
+// JS PROMISE .THEN .CATCH
+console.log('js promise');
+const myPromise = new Promise(function (onsolve, onreject) {
+    console.log('In promise');
+    let req = new XMLHttpRequest();
+    req.open('GET', "./mycar.html");
+    console.dir(req);
+    req.onload = function () {
+        if (req.status === 200) {
+            onsolve(req.response);
+        }
+        else {
+            onreject(new Error('File not found!'))
+        }
+    }
+    req.send();
+});
+
+myPromise
+    .then((res) => {
+        display(res, 'show-pro');
+    })
+    .catch((err) => {
+        display(`<h1>${err.message}</h1>`,'show-pro');
+    });
