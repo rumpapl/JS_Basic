7a52be2b17ba1365f01a15e6e69e34408766589c
rumpapl <rumpakucse14@gmail.com>
2021-11-22 14:17:09 +0600

    js timing events example

--- JS Browser DOM/script.js
+++ JS Browser DOM/script.js
@@ -7,3 +7,38 @@ function popup() {
     Line-breaks in a popup box.`;
     alert(text);
 }
+
+// global variable
+var TimeoutId;
+var IntervalId;
+
+
+// setTimeout clearTimeout example
+const startTime = () => {
+    TimeoutId = setTimeout(function(){console.log("Start");}, 2000);
+    console.log("wait");
+}
+
+const stopAlert = () =>{
+    clearTimeout(TimeoutId);
+    console.log("Stop");
+} 
+
+// setInterval clearIntervalexample
+const startTimeInterval = () => {
+    IntervalId = setInterval(function(){console.log("Start");}, 1000);
+    
+}
+
+const stopTimeInterval = () =>{
+    clearInterval(IntervalId);
+    console.log("Stop");
+} 
+
+
+
+
+
+
+
+
